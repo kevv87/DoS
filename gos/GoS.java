fdd3679570b6f61feaa968cc3a3b15648248d5e0
kevv87 <7m6JHC84iMsw>
2018-10-09 09:08:27 -0600

    Cambios de craforsh

---
+++ gos/GoS.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gos;
+
+/**
+ *
+ * @author kevv87
+ */
+public class GoS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
